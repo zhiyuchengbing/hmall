01a744c9668a3f49b8091d0591447d34aeb61176
zhiyuchengbing <152857110+zhiyuchengbing@users.noreply.github.com>
2025-11-05 15:48:23 +0800

    微服务项目
    微服务第一阶段

---
+++ cart-service/src/main/java/com/hmall/cart/mapper/CartMapper.java
@@ -0,0 +1,26 @@
+package com.hmall.cart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import com.hmall.cart.domain.po.Cart;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+/**
+ * <p>
+ * 购物车 Mapper 接口
+ * </p>
+ */
+@Mapper
+public interface CartMapper extends BaseMapper<Cart> {
+
+    /**
+     * 更新购物车中指定商品的数量 +1
+     *
+     * @param itemId 商品ID（Long类型）
+     * @param userId 用户ID（Long类型）
+     */
+    @Update("UPDATE cart SET num = num + 1 WHERE user_id = #{userId} AND item_id = #{itemId}")
+    void updateNum(@Param("itemId") Long itemId, @Param("userId") Long userId);
+}
---
+++ pay-service/src/main/java/com/hmall/pay/PayApplication.java
@@ -0,0 +1,21 @@
+package com.hmall.pay;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("com.hmall.pay.mapper")
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.hmall.api.client")
+public class PayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
