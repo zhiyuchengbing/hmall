e6185d37f7ca4f988ae0e9a1e17fe7d39fa0234b
zhiyuchengbing <152857110+zhiyuchengbing@users.noreply.github.com>
2025-11-06 17:04:56 +0800

    2025-11-06

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/MyGlobalFilter.java
@@ -0,0 +1,19 @@
+package com.hmall.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyGlobalFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //TODo 模拟登录逻辑
+        
+        return chain.filter(exchange);
+    }
+
+}
